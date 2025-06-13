fa551b1db4e8b7f0fb892b1e843492572de0f859
MAURO2024-hash <maurice20211@outlook.com>
2025-06-13 03:11:38 +0200

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  basePath: '/site-cosmetech-25',
+  assetPrefix: '/site-cosmetech-25'
+}
+
+module.exports = nextConfig
